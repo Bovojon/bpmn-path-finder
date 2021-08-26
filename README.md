a8a28f871bd8f6d2213aadea3d5e9635059e7648
bovojon <bovojon@gmail.com>
2021-08-26 19:26:59 +0300

    Implement BFS to find path between start and end nodes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # BPMN Path Finder
+
+For example, to test with `approveInvoice` as the start and `invoiceProcessed` as the end:
+
+```
+java -jar target/path_finder-1.0-jar-with-dependencies.jar approveInvoice invoiceProcessed
+```
--- src/main/java/bpmn/project/App.java
+++ src/main/java/bpmn/project/App.java
@@ -8,9 +8,17 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import com.google.gson.Gson;
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.bpmn.instance.SequenceFlow;
 import org.camunda.bpm.model.bpmn.instance.FlowNode;
 
 class Foo {
@@ -22,6 +30,8 @@ public class App {
   private static String url = "https://n35ro2ic4d.execute-api.eu-central-1.amazonaws.com/prod/engine-rest/process-definition/key/invoice/xml";
 
   public static void main(String[] args) throws IOException, InterruptedException {
+    String startID = args[0];
+    String endID = args[1];
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(url))
@@ -33,7 +43,41 @@ public class App {
     Foo obj = gson.fromJson(response.body(), Foo.class);
     InputStream stream = new ByteArrayInputStream(obj.bpmn20Xml.getBytes(Charset.forName("UTF-8")));
     BpmnModelInstance modelInstance = Bpmn.readModelFromStream(stream);
-    FlowNode startNode = (FlowNode) modelInstance.getModelElementById("approveInvoice");
-    System.out.println((startNode.getName()));
+    FlowNode startNode = (FlowNode) modelInstance.getModelElementById(startID);
+    List<String> path = new ArrayList<>();
+
+    Map<String, Boolean> visited = new HashMap<>();
+    Map<String, String> backtracking = new HashMap<>();
+    Queue<FlowNode> q = new ArrayDeque<>();
+    visited.put(startID, true);
+    q.add(startNode);
+    
+    while(!q.isEmpty()) {
+      FlowNode node = q.remove();
+      if (node.getId().equals(endID)) {
+        String tempStartID = "";
+        String tempEndID = endID;
+        path.add(endID);
+        while (!tempStartID.equals(startID)) {
+          tempStartID = backtracking.get(tempEndID);
+          path.add(0, tempStartID);
+          tempEndID = tempStartID;
+        }
+        System.out.println("The path from approveInvoice to invoiceProcessed is:");
+        System.out.println(path);
+        return;
+      }
+      Collection<SequenceFlow> edges = node.getOutgoing();
+      for (SequenceFlow edge : edges) {
+        FlowNode adjacentNode = edge.getTarget();
+        backtracking.put(adjacentNode.getId(), node.getId());
+        if (visited.get(adjacentNode.getId()) == null || !visited.get(adjacentNode.getId())) {
+          visited.put(adjacentNode.getId(), true);
+          q.add(adjacentNode);
+        }
+      }
+    }
+    System.out.println(-1);
+    System.exit(-1);
   }
 }
